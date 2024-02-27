047101ee25414ea73adb9e97cb55093af34d16e6
Dopezapha <officialnwaneridaniel@gmail.com>
2024-02-27 05:34:47 -0800

    The regular expression must match School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,10 +1,3 @@
 #!/usr/bin/env ruby
 # Get the input argument from the command line
-input = ARGV[0]
-
-pattern = /^[A-Z]+ [a-z]+$/
-if input =~ pattern
-  puts "Input matches the pattern: #{input}"
-else
-  puts "Input does not match the pattern: #{input}"
-end
+/^[A-Z]+ [a-z]+$/
