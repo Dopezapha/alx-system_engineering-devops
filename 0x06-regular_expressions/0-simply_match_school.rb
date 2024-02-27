53ab3d4444a1d60da54202915d25793bbca95e8a
Dopezapha <officialnwaneridaniel@gmail.com>
2024-02-27 05:29:11 -0800

    The regular expression must match School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,3 @@
 #!/usr/bin/env ruby
 # Get the input argument from the command line
-input = ARGV[0]
-
-# Define the regular expression pattern to match "School"
-pattern = /School/
-
-# Check if the input matches the pattern
-if input =~ pattern
-  puts "Input matches 'School'."
-else
-  puts "Input does not match 'School'."
-end
+[A-B]+ [a-b]+
