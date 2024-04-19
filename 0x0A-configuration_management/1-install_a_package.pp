3d55b5eaa193740394dc9f0fbe646e2426482638
Dopezapha <officialnwaneridaniel@gmail.com>
2024-04-19 09:06:33 -0700

    Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Puppet manifest to install Flask version 2.1.0 on Ubuntu 20.04 LTS
-package { 'flask==2.1.0':
-  ensure   => 'installed',
+package { 'flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
 
