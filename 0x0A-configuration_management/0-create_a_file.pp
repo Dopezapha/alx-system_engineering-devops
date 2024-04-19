14e6bc58ef5ae51277fb6f4259ca36e5c7440b5d
Dopezapha <officialnwaneridaniel@gmail.com>
2024-04-19 06:10:06 -0700

    create a file in /tmp using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# Using Puppet, create a file in /tmp.
 file { '/tmp/school':
   ensure  => 'file',
-  content => "I live puppet\n",
+  mode => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
+  content => "I love puppet\n",
 }
