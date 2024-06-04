783f5e29ef42e6bec4a25b87dd66219954d7d0e2
root <root@DESKTOP-MGUKQKN>
2024-06-05 00:32:08 +1000

    a function that queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Get subreddit subscriber count"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Get the number of subscribers for a given subreddit.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}  # Provide a user agent to avoid 429 error
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
